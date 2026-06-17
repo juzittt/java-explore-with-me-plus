8e8a1a5357de9fe52041dad96f04c91fda16f75e
Dmitry Uglanov <dimarik.pay2009@yandex.ru>
2026-06-17 18:07:35 +0300

    стартовый коммит для разработки сервиса

---
+++ stats/stats-dto/src/main/java/ewm/StatsDto.java
@@ -0,0 +1,18 @@
+package ewm;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatsDto {
+    private String app;
+
+    private String uri;
+
+    private long hits;
+}
