ff0b53cfe8744f4aa30e4eb5926d5f60ac619650
Igor Tumanov <ihortum@yandex.ru>
2026-06-24 22:02:36 +0300

    add Specification for filters

---
+++ main-service/src/main/java/ewm/events/repository/EventsRepository.java
@@ -0,0 +1,21 @@
+package ewm.events.repository;
+
+import ewm.events.model.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+/*
+Решил сделать через Specification,
+чтоб упростить работу, а то слишком много фильтров
+*/
+public interface EventsRepository extends
+        JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+
+    Page<Event> findByInitiatorId(Long initiatorId, Pageable pageable);
+
+    Optional<Event> findByIdAndInitiatorId(Long eventId, Long initiatorId);
+}
