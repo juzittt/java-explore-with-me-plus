f962c91e67c20937895bf955b386f99e08310260
SergeyKudrin84 <conan4@yandex.ru>
2026-06-25 14:01:29 +1000

    Добавлено обогащение ConfirmedRequests и Views у EventFullDto

--- main-service/src/main/java/ewm/events/dto/params/AdminEventParams.java
+++ main-service/src/main/java/ewm/events/dto/params/AdminEventParams.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,8 +17,10 @@ public class AdminEventParams {
     private List<Long> users;
     private List<String> states;
     private List<Long> categories;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime rangeStart;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime rangeEnd;
-    private Integer from;
-    private Integer size;
+    private Integer from = 0;
+    private Integer size = 10;
 }
