8b2e46718f87de05d8a37dd334790b828f5a0e60
Igor Tumanov <ihortum@yandex.ru>
2026-07-06 18:44:52 +0300

    add comment model

---
+++ main-service/src/main/java/ewm/comments/model/Comment.java
@@ -0,0 +1,40 @@
+package ewm.comments.model;
+
+import ewm.events.model.Event;
+import ewm.users.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2000)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(name = "created_on", nullable = false)
+    private LocalDateTime createdOn;
+
+
+}
